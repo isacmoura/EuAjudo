11a492fb2a4b2eaf3828a5224d0777c29fe73dfa
Pedro Igor <pedro.igor.ps@alu.ufc.br>
2020-08-26 05:52:56 -0300

    Alterado os nomes de uns arquivos , ajustados alguns pontos do texto.

--- frontend/scripts/pagina-registro-user.js
+++ frontend/scripts/pagina-registro-user.js
@@ -15,24 +15,35 @@ function popularUFs() {
 
 function getCidades(event) {
     const citySelect = document.querySelector("select[name=cidade]")
-    const stateInput = document.querySelector("input[name=estado]")
+    
+    /*const stateInput = document.querySelector("input[name=estado]")
 
     const ufValue = event.target.value
     
     const indexOfSelectedState = event.target.selectedIndex
 
-    stateInput.value = event.target.options[indexOfSelectedState].text
+    stateInput.value = event.target.options[indexOfSelectedState].text*/
 
-    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/municipios`
 
     fetch(url)
-    .then( (res) => {res.json()} )
-    .then((cities) => {
+    .then(async (res) => {
+        const dados = await res.json() 
+        for (const cidade of dados) {
+            var opt = document.createElement('option');
+            opt.id = cidade.id;
+            opt.value = cidade.nome;
+            opt.innerHTML = cidade.nome;
+            citySelect.appendChild(opt);
+        }
+        citySelect.disabled = false
+    })
+    /*.then((cities) => {
         for(const cidade of cities) {
             citySelect.innerHTML += `<option value="${cidade.id}"> ${cidade.name}<\option>`
         }
         citySelect.disabled = false
-    }) 
+    })*/ 
 }
 
 document
